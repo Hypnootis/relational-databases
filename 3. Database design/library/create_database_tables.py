192099eb4fa49b7c7d9b50c4bce727f6e6cc307d
hypnootis <tm.pasanen@outlook.com>
2022-10-09 20:00:21 +0300

    Finally able to create database

--- 3. Database design/library/create_database_tables.py
+++ 3. Database design/library/create_database_tables.py
@@ -1,12 +1,31 @@
 import psycopg2
 
+# To resolve the path for the config file
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
+from database_config import username, password, host, port
+
 def create_database():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="",
-        user="",
-        password=""
+    connection = psycopg2.connect(
+        host = host,
+        database = "postgres",
+        user = username,
+        password = password,
+        port = port
     )
+
+    connection.autocommit = True
+
+    cursor = connection.cursor()
+
+    
+    commands = "CREATE DATABASE library"
+
+    cursor.execute(commands)
+    connection.close()
 
 def create_tables():
     print("hello")
+
+create_database()
---
+++ database_config_template.py
@@ -0,0 +1,7 @@
+# Modify this template to suit your needs for creating the database.
+# First, rename this file to just "database-config.py"
+# Then, modify these variables appropriately:
+
+username = ""
+password = ""
+host = ""
